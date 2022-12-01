31e7d795305ebd1bff278537cc26abced32ee9a2
ChiefChippy2 <chiefchippy2@gmail.com>
2022-12-01 23:37:46 +0100

    fixed get_input: doesnt override content

--- get_input.pl
+++ get_input.pl
@@ -4,9 +4,9 @@ print"Enter Day [$d]: ";
 $a=<>=~/\d+/?$&:$d;
 print"Enter filename [input$d.txt]: ";
 $b=<>=~/^\S+/?$&:"input$d.txt";
-print`curl -o ./$b --cookie "session=$AUTH" https://adventofcode.com/2022/day/$a/input`;
+print`curl -o ./$b --cookie "session=\$AUTH" https://adventofcode.com/2022/day/$a/input`;
 print'Update piston.sh? [Y/n]';
 $c=<>;chomp$c;
 if(($c=~/n/i)){die;}
 print`sed -E 's/[0-9]+/$a/g' -i piston.sh`;
-print`> day$a.pl`
+print`>> day$a.pl`
